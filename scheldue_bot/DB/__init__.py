009dbdfeedb5a47763d30e48df48487067acb300
rezschikovivan <rezschikovivan@gmail.com>
2026-05-18 21:31:23 +0300

    Чистка

--- scheldue_bot/DB/__init__.py
+++
@@ -1,4 +0,0 @@
-from .loader import set_role, User, UserRole, Roles, get_or_create_user
-from .verification import CheckStatus
-
-__all__ = ["set_role", "User", "UserRole", "Roles", "CheckStatus", "get_or_create_user"]
